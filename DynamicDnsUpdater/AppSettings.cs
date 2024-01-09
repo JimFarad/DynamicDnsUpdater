44d995ef46d812bef869d03de5d9ff4205035f29
gfarantouris <gfarantouris@Q-RIOSITY>
2024-01-09 13:59:45 +0200

    Add project files.

---
+++ DynamicDnsUpdater/AppSettings.cs
@@ -0,0 +1,33 @@
+﻿namespace DynamicDnsUpdater
+{
+    static class AppSettings
+    {
+        public enum logSettingEnum { noLog = 0, logTemp, logHere };
+
+        //Settings from file
+        public static bool firstRun = true;//if file doesn't exist
+        public static string user = "";
+        public static string password = "";
+        public static string hostname = "";
+        public static string updateLink = "";
+        public static uint updateInterval = 1;
+        public static logSettingEnum logSetting = logSettingEnum.noLog;
+        public static bool checkAlsoLocalIpChange = true;
+
+        //Settings from cmdLine
+        public static bool overrideLogOption = false; //if true, we have log options from cmdline, so we ignore the one from file
+        public static logSettingEnum originalLogSetting = logSettingEnum.noLog;//we need to keep a copy of the original setting in case someone save new settings with override log enabled
+
+        //settings from regedit / SchTasks
+        public static bool autorunUserEnabled = false;
+        public static bool autorunAdminEnabled = false;
+
+        //Settings internal
+        public static bool exitUpdateLoop = false;
+        public static utilityFunctions.UpdateStatus lastUpdateStatus = utilityFunctions.UpdateStatus.NotConnected;
+        public static bool lastUpdateStatusChanged = false;
+        public static string logFileName = "ddu.log";
+        public static string settingsFileName = "dduSettings.txt";
+
+    }
+}
---
+++ DynamicDnsUpdater/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace DynamicDnsUpdater
+{
+    static class Program
+    {
+        static utilityFunctions utils = new utilityFunctions();
+        static bool backGroundMode = false;
+        /// <summary>
+        /// Check command line arguments and calls appropriate functions
+        /// </summary>
+        /// <param name="args">cmd line agruments</param>
+        static bool ParseCmdLine(string[] args)
+        {
+            bool help = false, continueRunProgram = true;
+            foreach (string arg in args)
+            {
+                string arglow = arg.ToLower();//case insensitive
+                arglow = arglow.Replace('/', '-');//both allowed
+
+                switch (arglow)
+                {
+                    case "-nolog":
+                        if (AppSettings.overrideLogOption == false)
+                        {
+                            AppSettings.logSetting = AppSettings.logSettingEnum.noLog;
+                            AppSettings.overrideLogOption = true;
+                        }
+                        break;
+                    case "-log":
+                    case "-logtemp":
+                        if (AppSettings.overrideLogOption == false)
+                        {
+                            AppSettings.logSetting = AppSettings.logSettingEnum.logTemp;
+                            AppSettings.overrideLogOption = true;
+                        }
+                        break;
+                    case "-loghere":
+                        if (AppSettings.overrideLogOption == false)
+                        {
+                            AppSettings.logSetting = AppSettings.logSettingEnum.logHere;
+                            AppSettings.overrideLogOption = true;
+                        }
+                        break;
+                    case "-bg":
+                        backGroundMode = true;//runs in background mode (do not start the form)
+                        break;
+                    case "-help":
+                        help = true;
+                        continueRunProgram = false;
+                        break;
+                    default:
+                        MessageBox.Show("Invalid command, try \"-help\". Error at argument: \"" + arglow + "\"");
+                        continueRunProgram = false;
+                        break;
+                }
+            }
+            if (help == true) //showed only once
+                MessageBox.Show(
+                    "-help\tShow this message\n" +
+                    "Note: if you specify a log option the corresponding file setting is ignored;\nlog setting in file is not changed\n" +
+                    "-NoLog\tDisable log\n" +
+                    "-LogTemp\tEnable logging to file %TEMP%\\" + AppSettings.logFileName + "\n" +
+                    "-LogHere\tEnable logging to file in application directory\n" +
+                    "-bg\tStarts in background mode (no GUI)"
+
+                    , "Available commands (case insensitive):");
+            return continueRunProgram;
+        }
+
+        /// <summary>
+        /// Punto di ingresso principale dell'applicazione.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            //Embedded dll, avoid dependency
+            AppDomain.CurrentDomain.AssemblyResolve += (sender, arg) => { if (arg.Name.StartsWith("Interop.TaskScheduler")) return Assembly.Load(Properties.Resources.Interop_TaskScheduler); return null; }; //embedded dependency
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            //Enforce using Tls1.2 or 1.3 before any connection (Tls 1.0 and 1.1 are disabled)
+            System.Net.ServicePointManager.SecurityProtocol = System.Net.SecurityProtocolType.Tls13 | System.Net.SecurityProtocolType.Tls12;
+
+            //my pre-gui added code (command line)
+            if (args.Length != 0)
+                if (ParseCmdLine(args) == false)
+                    return;
+            if (utils.ReadSettings() == true)
+                AppSettings.firstRun = false;
+            utils.AddLog("Version: " + Application.ProductVersion);//here because if we log or not is readfrom file (or commandline)
+            if (backGroundMode == true)
+            {
+                utils.AddLog("Background mode");
+                if (AppSettings.firstRun == true)
+                    utils.AddLog("Can't start in background mode without settings file"); //will go to "program closed"                
+                else
+                    utils.WaitAndUpdate(); //program will never exit/return from here
+            }
+            else
+            {
+                utils.AddLog("GUI mode");
+                Application.Run(new MainForm());
+            }
+            utils.AddLog("Process closed");
+        }
+    }
+}
